e1c2fa4bed0831066826ccd1a51a6fe758c938a9
MANOJ KUMAR <104015866+MARCUS-00@users.noreply.github.com>
2023-06-26 01:49:15 +0530

    Update supabase.ts

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -4,7 +4,7 @@ import { Database } from "../../types/supabase";
 
 
 
-NEXT_PUBLIC_SUPABASE_URL='https://mhrttdcgoqsdswpocase.supabase.co'
-NEXT_PUBLIC_SUPABASE_ANON_KEY="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1ocnR0ZGNnb3FzZHN3cG9jYXNlIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODc3MTIxOTksImV4cCI6MjAwMzI4ODE5OX0.WUEGFzJViFwqDD-kFdZdy1moSI3wpUUVAoa5C5L6_Bk"
+const supa_url ='https://mhrttdcgoqsdswpocase.supabase.co'
+const supa_key ="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1ocnR0ZGNnb3FzZHN3cG9jYXNlIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODc3MTIxOTksImV4cCI6MjAwMzI4ODE5OX0.WUEGFzJViFwqDD-kFdZdy1moSI3wpUUVAoa5C5L6_Bk"
 
 export const SupaClient = createClient<Database>(supa_url,supa_key);
